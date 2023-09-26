6ea683c7d19d8eb190ec9643006ec9d295bbaa7d
Alexander Morugin <nobilis@bk.ru>
2023-09-27 00:19:47 +0300

    made models

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import mongoose from 'mongoose';
 
 const app = express();
 const { PORT = 3000 } = process.env;
+const URL = "mongodb://localhost:27017/mestodb";
+
+mongoose
+  .connect(URL)
+  .then(() => console.log("Успешное соединение с MongoDB"))
+  .catch((err) => console.log(`Произошла ошибка при подсоединении к MongoDB: ${err}`))
 
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порте http://localhost:${PORT}`)
-})
+  console.log(`Сервер запущен на порте http://localhost:${PORT}`);
+});
