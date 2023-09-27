d31415bc3578ff3691ac579ec8e2a9ef791237c2
Alexander Morugin <nobilis@bk.ru>
2023-09-27 18:32:51 +0300

    подключил mongoose, все работает кроме post

--- src/app.ts
+++ src/app.ts
@@ -2,127 +2,119 @@ import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user";
 
-import { connectToDb, getDb } from "./db";
-import { ObjectId } from "mongodb"
+// import { connectToDb, getDb } from "./db";
+// import { ObjectId } from "mongodb";
+import User from "./models/user"
+
+const { PORT = 3000 } = process.env;
+const URL = "mongodb://localhost:27017/mestodb";
 
 const app = express();
-app.use(express.json())
-const { PORT = 3000 } = process.env;
+app.use(express.json());
 
-let db: any;
+mongoose
+  .connect(URL)
+  .then(() => console.log("Успешное соединение с MongoDB"))
+  .catch((err) =>
+    console.log(`Произошла ошибка при подсоединении к MongoDB: ${err}`)
+  );
 
-connectToDb((err: any) => {
-  if (!err) {
-    app.listen(PORT, () => {
-      console.log(`Сервер запущен на порте http://localhost:${PORT}`);
-    });
-    db = getDb();
-  } else {
-    console.log(`Произошла ошибка подключения к серверу: ${err}`)
-  }
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порте http://localhost:${PORT}`);
 });
 
-app.get("/users", (req, res) => {
-  const users:any  = [];
+// let db: any;
 
-  db
-    .collection("users")
+// connectToDb((err: any) => {
+//   if (!err) {
+//     // app.listen(PORT, () => {
+//     //   console.log(`Сервер запущен на порте http://localhost:${PORT}`);
+//     // });
+//     db = getDb();
+//   } else {
+//     console.log(`Произошла ошибка подключения к серверу: ${err}`);
+//   }
+// });
+
+app.get("/users", (req, res) => {
+  // const users: any = [];
+
+  User
+    // .collection("users")
     .find()
     // .sort({ name: -1 })
-    .forEach((user: any) => users.push(user))
-    .then(() => {
-      res
-        .status(200)
-        .json(users)
+    // .forEach((user: any) => users.push(user))
+    .then((users) => {
+      res.status(200).json(users);
     })
     .catch(() => {
-      res
-        .status(500)
-        .json({error: "Что то пошло не так..."})
-    })
-})
+      res.status(500).json({ error: "Что то пошло не так..." });
+    });
+});
 
 app.get("/users/:id", (req, res) => {
-  if (ObjectId.isValid(req.params.id)) {
-    db
-    .collection("users")
-    .findOne({ _id: new ObjectId(req.params.id) })
-    .then((doc: any) => {
-      res
-        .status(200)
-        .json(doc)
-    })
-    .catch(() => {
-      res
-        .status(500)
-        .json({error: "Что то пошло не так..."})
-    })
-  } else {
-    res
-    .status(500)
-    .json({error: "ID Wrong"})
-  }
-})
+  // if (ObjectId.isValid(req.params.id)) {
+    User
+      // .collection("users")
+      // .findOne({ _id: new ObjectId(req.params.id) })
+      .findById(req.params.id)
+      .then((user) => {
+        res.status(200).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({ error: "Что то пошло не так..." });
+      });
+  // } else {
+  //   res.status(500).json({ error: "ID Wrong" });
+  // }
+});
 
 app.delete("/users/:id", (req, res) => {
-  if (ObjectId.isValid(req.params.id)) {
-    db
-    .collection("users")
-    .deleteOne({ _id: new ObjectId(req.params.id) })
-    .then((result: any) => {
-      res
-        .status(200)
-        .json(result)
-    })
-    .catch(() => {
-      res
-        .status(500)
-        .json({error: "Что то пошло не так..."})
-    })
-  } else {
-    res
-    .status(500)
-    .json({error: "ID Wrong"})
-  }
-})
+  // if (ObjectId.isValid(req.params.id)) {
+    User
+      // .collection("users")
+      .findByIdAndDelete(req.params.id)
+      .then((result: any) => {
+        res.status(200).json(result);
+      })
+      .catch(() => {
+        res.status(500).json({ error: "Что то пошло не так..." });
+      });
+  // } else {
+  //   res.status(500).json({ error: "ID Wrong" });
+  // }
+});
 
 app.post("/users", (req, res) => {
-  db
-  .collection("users")
-  .insertOne(req.body)
-  .then((result: any) => {
-    res
-      .status(201)
-      .json(result)
-  })
-  .catch(() => {
-    res
-      .status(500)
-      .json({error: "Что то пошло не так..."})
-  })
-})
+  const postUser = new User(req.body);
 
-app.patch("/users/:id", (req, res) => {
-  if (ObjectId.isValid(req.params.id)) {
-    db
-    .collection("users")
-    .updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body })
+  postUser
+    .save()
     .then((result: any) => {
       res
-        .status(200)
-        .json(result)
+        .status(201)
+        .json(result);
     })
     .catch(() => {
-      res
-        .status(500)
-        .json({error: "Что то пошло не так..."})
-    })
-  } else {
-    res
-    .status(500)
-    .json({error: "ID Wrong"})
-  }
-})
+      res.status(500).json({ error: "Что то пошло не так..." });
+    });
+});
+
+app.patch("/users/:id", (req, res) => {
+  // if (ObjectId.isValid(req.params.id)) {
+    User
+      // .collection("users")
+      .findByIdAndUpdate(req.params.id, req.body)
+      .then((result: any) => {
+        res.status(200).json(result);
+      })
+      .catch(() => {
+        res.status(500).json({ error: "Что то пошло не так..." });
+      });
+  // } else {
+  //   res.status(500).json({ error: "ID Wrong" });
+  // }
+});
 
 // const URL = "mongodb://localhost:27017/mestodb";
 
@@ -130,10 +122,5 @@ app.patch("/users/:id", (req, res) => {
 // app.use(express.urlencoded({ extended: true }));
 
 // mongoose.connect(URL);
-
-// mongoose
-//   .connect(URL)
-//   .then(() => console.log("Успешное соединение с MongoDB"))
-//   .catch((err) => console.log(`Произошла ошибка при подсоединении к MongoDB: ${err}`))
 
 // app.use('/users', userRouter);
--- src/db.js
+++ src/db.js
@@ -1,21 +1,21 @@
-const { MongoClient } = require("mongodb")
+// const { MongoClient } = require("mongodb")
 
-const URL = "mongodb://localhost:27017/mestodb";
+// // const URL = "mongodb://localhost:27017/mestodb";
 
-let dbConnection;
+// let dbConnection;
 
-module.exports = {
-  connectToDb: (cb) => {
-    MongoClient
-      .connect(URL)
-      .then((client) => {
-        console.log("Connected to MongoDB")
-        dbConnection = client.db()
-        return cb()
-      })
-      .catch((err) => {
-        return cb(err)
-      })
-  },
-  getDb: () => dbConnection
-}
+// module.exports = {
+//   connectToDb: (cb) => {
+//     MongoClient
+//       .connect(URL)
+//       .then((client) => {
+//         console.log("Connected to MongoDB")
+//         dbConnection = client.db()
+//         return cb()
+//       })
+//       .catch((err) => {
+//         return cb(err)
+//       })
+//   },
+//   getDb: () => dbConnection
+// }
